4aec7550affe073d12704c1dd4ba0c2b429591a7
murcast <murcast.zero@gmail.com>
2023-11-10 00:23:40 +0200

    kafka stream map only value of PromotionMessage code

---
+++ src/main/java/com/course/kafka/broker/stream/promotion/PromotionUppercaseJsonStream.java
@@ -0,0 +1,42 @@
+package com.course.kafka.broker.stream.promotion;
+
+import com.course.kafka.message.PromotionMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class PromotionUppercaseJsonStream {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Bean
+    public KStream<String, String> promotionUppercaseJsonStreamBean(StreamsBuilder builder) {
+        var string = Serdes.String();
+        var sourceStream = builder.stream("t-commodity-promotion", Consumed.with(string, string));
+        var resultStream = sourceStream.mapValues(this::uppercasePromotionCode);
+
+        resultStream.to("t-commodity-promotion-uppercase");
+
+        sourceStream.print(Printed.<String, String>toSysOut().withLabel("JSON original stream"));
+        resultStream.print(Printed.<String, String>toSysOut().withLabel("JSON uppercase stream"));
+
+        return resultStream;
+    }
+
+    @SneakyThrows
+    private String uppercasePromotionCode(String message) {
+        var promotionMessage = objectMapper.readValue(message, PromotionMessage.class);
+        return objectMapper.writeValueAsString(new PromotionMessage(promotionMessage.getPromotionCode().toUpperCase()));
+    }
+}
